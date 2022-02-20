e2453cacba3613b2b457255e62fc54a442be7411
chongada <66376380+chongada@users.noreply.github.com>
2022-02-20 22:05:15 +0100

    Update networks.js

--- src/networks.js
+++ src/networks.js
@@ -1,24 +1,24 @@
 module.exports = {
     qtum: {
-        messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'qc',
+        messagePrefix: '\x15Butk Signed Message:\n',
+        bech32: 'bc',
         bip32: {
             public: 0x0488b21e,
             private: 0x0488ade4
         },
-        pubKeyHash: 0x3a,
-        scriptHash: 0x32,
-        wif: 0x80
+        pubKeyHash: 0x4c,
+        scriptHash: 0x10,
+        wif: 0xc
     },
     qtum_testnet: {
         messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'tq',
+        bech32: 'tb',
         bip32: {
             public: 0x043587cf,
             private: 0x04358394
         },
-        pubKeyHash: 0x78,
-        scriptHash: 0x6e,
+        pubKeyHash: 0x8c,
+        scriptHash: 0x13,
         wif: 0xef
     }
 }
